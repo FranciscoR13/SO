4dfcc50d46ab72e3843a9a4faa51d39dbb9b1df1
FranciscoR13 <120480144+FranciscoR13@users.noreply.github.com>
2024-11-09 17:07:31 +0000

    Update feed.c

--- feed.c
+++ feed.c
@@ -21,41 +21,33 @@ int main(int argc, char* argv[]) {
     int mp, up, n;
     pid_t pid = getpid();
 
-    //FIFO QUE RECEBE MENSAGENS DO MANAGER
-    snprintf(upid, sizeof(upid), "%d", pid); // Gera o nome do FIFO exclusivo usando o PID
-    mkfifo(upid, 0600); // Cria o FIFO exclusivo para leitura de resposta
+    //USER-PIPE
+    snprintf(upid, sizeof(upid), "%d", pid);     // Gera o nome do FIFO exclusivo usando o PID
+    mkfifo(upid, 0600);                          // Cria o FIFO exclusivo para leitura de resposta
+    up = open(upid, O_RDONLY);                   // Abre o FIFO exclusivo para receber mensagens do manager.c
 
-    // Abre o FIFO principal para comunicação com o gerenciador
+    // MANAGER-PIPE
     mp = open(FIFO_CS, O_WRONLY);
 
-    // Envia o PID para o manager.c
-    write(mp, upid, strlen(upid) + 1);
-
-    // Abre o FIFO exclusivo para receber mensagens do `manager.c`
-    up = open(upid, O_RDONLY);
-
-    // Lê a resposta de confirmação do manager.c
-    n = read(up, cmd, sizeof(cmd) - 1);
+    //ENVIA E RECEBE A RESPOSTA DO MANAGER
+    write(mp, upid, strlen(upid) + 1);           // Envia o PID para o manager.c
+    n = read(up, cmd, sizeof(cmd) - 1);          
     if (n > 0) {
         cmd[n] = '\0';
         printf("LI... '%s' (%d bytes)\n", cmd, n);
     }
 
-    // Loop de envio de comandos ao manager.c
+    
+    // LOOP DE MSG PARA O MANAGERS
     do {
         scanf("%s", cmd);
         write(mp, cmd, strlen(cmd) + 1);
     } while (strcmp(cmd, "fim") != 0);
 
-    // Fecha e remove o FIFO
+    //FECHAR CENAS 
     close(mp);
     close(up);
-    // Remove o FIFO
-    if (unlink(upid) == -1) {
-        perror("Erro ao remover o FIFO");
-    } else {
-        printf("FIFO '%s' removido com sucesso.\n", upid);
-    }
+    unlink(upid);//NAO FUNCIONA FALAR COM O PROF...
 
     printf("FIM FEED...\n");
 
