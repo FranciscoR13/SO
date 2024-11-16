696ebffffab340525d4a76b20f44f3c9df0e88e0
Alexsosr21 <alexcunha03@gmail.com>
2024-11-16 15:47:00 +0000

    Update feed.c

--- feed.c
+++ feed.c
@@ -1,33 +1,4 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-
-#define TAM 50
-#define FIFO_CS "M-PIPE"
-
-/*
-#define MAX_TOPICOS 20 //definir a quantidade de topicos que podem existir
-
-
-struct Msg
-{
-    char corpo_msg[300];
-    int duracao; //a msg tem uma duração por isso pus isto aqui pode ser sujeito a alterações
-
-};
-
-
-struct Topico
-{
-    char nomeTopico[20];
-    char msg_persistentes[5][300];// 5 mensagens com 300 de comprimento
-    int bloqueado; // 0- desbloqueado 1-bloqueado | podemos usar bool em vez de int
-    struct Msg msgs[5];
-};
+#include "includes.h"
 
 
 // Comando 'topics': mostra todos os tópicos, se não existirem topicos da erro
