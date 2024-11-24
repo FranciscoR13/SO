04915afc0aa9995c0f68dd28d246117d32244592
Francisco Rodrigues <120480144+FranciscoR13@users.noreply.github.com>
2024-11-24 18:17:52 +0000

    Update feed.c

--- feed.c
+++ feed.c
@@ -14,6 +14,36 @@ void listar_topicos(int num_topicos) {
 
 
 
+#include "includes.h"
+
+// FUNCAO DE LOGIN QUE ENVIA O USERNAME E O PID
+bool login(int man_pipe, int feed_pipe, char* username, int pid) {
+
+    //VARIAVEIS
+    int tam;
+    LOGIN l;
+    RESPOSTA r;
+
+    // ESTRURA LOGIN
+    strncpy(l.username, username, sizeof(l.username) - 1);
+    l.username[sizeof(l.username) - 1] = '\0'; // Garantir que a string seja terminada
+    l.pid = pid;
+
+    //TENTA ENVIAR O LOGIN
+    tam = write(man_pipe, &l, sizeof(LOGIN));
+    if (tam < sizeof(LOGIN)) {
+        return false;  // Se não escreveu todos os bytes
+    }
+
+    //TENTA RECEBER O LOGIN
+    tam = read(feed_pipe, &r, sizeof(RESPOSTA));
+    if (tam < sizeof(RESPOSTA)) {
+        return false;  // Se não leu todos os bytes esperados
+    }
+
+    return true;
+}
+
 int main(int argc, char* argv[]) {
     if (argc != 2) {
         return 1;
@@ -26,29 +56,38 @@ int main(int argc, char* argv[]) {
     MENSAGEM m;
     RESPOSTA r;
 
-    //se nao existir da erro
+    //EXISTE SERVER? SE NAO -> TERMINA
     if(access(FIFO_SERV,F_OK)!=0) {
         printf("[ERRO]-SERVER OFF\n");
-        exit(3);
+        exit(1);
     }
 
+    // MENSAGEM DE ABERTURA
     printf("INICIO FEED...\n");
-    printf("PID: %d\n", pid);
 
-    //FIFO QUE RECEBE MENSAGENS DO MANAGER
+    //FIFO QUE RECEBE MENSAGENS DO MANAGER + ABRE PIPES
     sprintf(fifo_feed, FIFO_CLI, pid);
     mkfifo(fifo_feed, 0600);
     feed_pipe = open(fifo_feed, O_RDWR);
     man_pipe = open(FIFO_SERV, O_WRONLY);
 
+    //LOGIN
+    if(!login(man_pipe,feed_pipe,argv[1],pid)) {
+        printf("\nERRO NO LOGIN!\n");
+        unlink(fifo_feed);
+        close(feed_pipe);
+        close(man_pipe);
+        exit(2);
+    }
+    printf("\nLOGIN CONCLUIDO!\nPID: %d | USERNAME: %s\n", pid, argv[1]);
+
+    // CICLO QUE ESCREVE MSG PARA O MANAGER (VAI PASSAR A SER UMA FUNCAO)
     do {
         //TOPICO?
         printf("\nTOPICO: ");
         fflush(stdout);
         fgets(topic, TOPIC, stdin);
         topic[strcspn(topic, "\n")] = '\0';
-
-        if(strcmp(topic,"quit")==0){break;}
 
         //MENSAGEM?
         printf("MENSAGEM: ");
@@ -75,12 +114,16 @@ int main(int argc, char* argv[]) {
 
     } while (strcmp(m.topic, "quit") != 0);
 
+    // FECHA PIPES & APAGA FIFO
     close(feed_pipe);
     close(man_pipe);
     unlink(fifo_feed);
 
+    printf("\nFIM FEED...\n");
 
-    printf("FIM FEED...\n");
+    return 0;
+}
+
 
     return 0;
 }
