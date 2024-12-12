56b248fc663b108c4f7d3aef7cff7c8608c2be1a
Francisco Rodrigues <120480144+FranciscoR13@users.noreply.github.com>
2024-12-12 21:26:47 +0000

    Update includes.h

--- includes.h
+++ includes.h
@@ -8,6 +8,7 @@
 #include <sys/stat.h>
 #include <signal.h>
 #include <pthread.h>
+#include <errno.h>
 
 //TAM MAX DE...
 #define TAM_NOME 20
@@ -21,17 +22,8 @@
 #define MAX_MSG_PER 5
 
 // FIFOS
-#define FIFO_SERV_LOG "M_PIPE_LOG"  //MANAGER-PIPE LOGINS
-#define FIFO_SERV "M_PIPE"          //MANAGER-PIPE
+#define FIFO_SERV "M_PIPE"      //MANAGER-PIPE
 #define FIFO_CLI "F_%d"             //FEED-PIPE(PID)
-
-// A PARTIDA APENAS USADA NO MANAGER
-typedef struct {
-    int nUsers;
-    int users_pids[MAX_USERS];
-    char users_names[MAX_USERS][TAM_NOME];
-    pthread_mutex_t *ptrinco;
-}DATA;
 
 typedef struct {
     int pid;
@@ -39,14 +31,14 @@ typedef struct {
 }LOGIN;
 
 typedef struct {
+    int pid;
     char str[TAM_MSG];
-    //...
 }RESPOSTA;
 
 typedef struct {
     char topico[TAM_TOPICO];
     char corpo_msg[TAM_MSG];
-    int duracao;      //duracao da msg
+    int duracao;
     int pid;
 }MENSAGEM;
 
@@ -55,6 +47,41 @@ typedef struct
     char nome_topico[TAM];
     int bloqueado; // 0- desbloqueado !0-bloqueado
 
-    char msgs_persistentes[MAX_MSG_PER][TAM_MSG];// 5 mensagens com 300 de comprimento
+    //char msgs_persistentes[MAX_MSG_PER][TAM_MSG];// 5 mensagens com 300 de comprimento
+
     MENSAGEM mensagens[MAX_MSG_PER];
+    int nMsgs;
+
+    int subescritos[MAX_USERS][TAM_NOME];
+    int nSubs;
+
 }TOPICO;
+
+typedef struct {
+    int tipo;   // TIPO DE ESTRUTURA
+    union {
+        LOGIN l;
+        MENSAGEM m;
+        RESPOSTA r;
+    };
+}PEDIDO;
+
+// >>>>>>>>>>>>>>TENTAR DIMINUIR O TAMANHO DAS ESTRUTURAS <<<<<<<<<<<<<<<<<
+typedef struct {
+
+    // LOGIN
+    int nUsers;
+    int users_pids[MAX_USERS];
+    char users_names[MAX_USERS][TAM_NOME];
+
+    // TOPICOS
+    TOPICO topicos[MAX_TOPICS];
+    int nTopicos;
+
+    // INTERRUPTOR
+    bool pedidos_on;
+
+    // TRINCO
+    pthread_mutex_t *ptrinco;
+
+}DATA;
