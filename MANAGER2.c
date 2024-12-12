b41aea0ab2221d3d06b3d90f6b7abc6dfaf1c03b
Francisco Rodrigues <120480144+FranciscoR13@users.noreply.github.com>
2024-12-12 18:03:53 +0000

    Update MANAGER2.c

--- MANAGER2.c
+++ MANAGER2.c
@@ -141,10 +141,89 @@ void *executa_login(void *data) {
 //...
 
 // FUNCAO DA THREAD_MSG QUE FICA A RECEBER AS MENSAGENS
-void *revecebe_msg(void *data) {
-    //VARIAVEIS
+void *recebe_msg(void *data) {
+    MENSAGEM m;
+    DATA *d = (DATA *)data;
 
-    //FIM VARIAVEIS
+    // Inicializa as estruturas de mensagens e tópicos
+    memset(m.topico, 0, TAM_TOPICO);
+    memset(m.corpo_msg, 0, TAM_MSG);
+    m.duracao = 0;
+
+    // Criação e abertura do FIFO para mensagens
+    if (mkfifo(FIFO_SERV_MSG, 0600) < 0 && errno != EEXIST) {
+        perror("Erro ao criar FIFO_SERV_MSG");
+        return NULL;
+    }
+    int man_pipe_msg = open(FIFO_SERV_MSG, O_RDWR);
+    if (man_pipe_msg < 0) {
+        perror("Erro ao abrir FIFO_SERV_MSG");
+        return NULL;
+    }
+
+    while (d->msg_on) {
+        int tam = read(man_pipe_msg, &m, sizeof(MENSAGEM));
+        if (tam <= 0) {
+            perror("Erro ao ler do FIFO_SERV_MSG");
+            break;
+        }
+
+        // Checa se a mensagem é para encerrar a thread
+        if (strcmp(m.topico, "desliga") == 0 && m.pid == getpid()) {
+            printf("Mensagem de desligamento recebida. Encerrando thread.\n");
+            break;
+        }
+
+        printf("Tópico recebido: %s\nMensagem recebida: %s\nDuração: %d\n",
+               m.topico, m.corpo_msg, m.duracao);
+
+        // Armazena a mensagem no tópico apropriado
+        int topico_encontrado = 0;
+        for (int i = 0; i < TAM_TOPICO; i++) {
+            if (strcmp(d->topicos[i].nome_topico, m.topico) == 0) {
+                topico_encontrado = 1;
+                for (int j = 0; j < MAX_MSG_PER; j++) {
+                    if (d->topicos[i].mensagens[j].corpo_msg[0] == '\0') {
+                        // Armazena a mensagem no slot disponível
+                        strcpy(d->topicos[i].mensagens[j].corpo_msg, m.corpo_msg);
+                        d->topicos[i].mensagens[j].duracao = m.duracao;
+                        break;
+                    }
+                }
+                break;
+            } else if (d->topicos[i].nome_topico[0] == '\0') {
+                // Registra um novo tópico
+                strcpy(d->topicos[i].nome_topico, m.topico);
+                strcpy(d->topicos[i].mensagens[0].corpo_msg, m.corpo_msg);
+                d->topicos[i].mensagens[0].duracao = m.duracao;
+                topico_encontrado = 1;
+                break;
+            }
+        }
+
+        if (!topico_encontrado) {
+            printf("[AVISO] Número máximo de tópicos atingido. Mensagem descartada.\n");
+        }
+
+        // Exibe os tópicos e mensagens armazenados
+        printf("\n[Estado Atual]\n");
+        for (int i = 0; i < TAM_TOPICO; i++) {
+            if (d->topicos[i].nome_topico[0] != '\0') {
+                printf("Tópico: %s\n", d->topicos[i].nome_topico);
+                for (int j = 0; j < MAX_MSG_PER; j++) {
+                    if (d->topicos[i].mensagens[j].corpo_msg[0] != '\0') {
+                        printf("  Mensagem: %s (Duração: %d)\n",
+                               d->topicos[i].mensagens[j].corpo_msg,
+                               d->topicos[i].mensagens[j].duracao);
+                    }
+                }
+            }
+        }
+    }
+
+    // Fechamento e limpeza
+    close(man_pipe_msg);
+    unlink(FIFO_SERV_MSG);
 
     return NULL;
 }
@@ -167,13 +246,29 @@ void mostra_users(DATA *d) {
     //printf("\n\n");
 }
 
+// MOSTRA OS USERNAMES E OS RESPETIVOS PIDS DE TODOS
+void mostra_topicos(DATA *d) {
+    pthread_mutex_lock(d->ptrinco);
+    int n = d->nTopicos;
+    pthread_mutex_unlock(d->ptrinco);
+
+    printf("\nTOPICOS(%d):\n", n);
+    for (int i = 0; i < n; i++) {
+        pthread_mutex_lock(d->ptrinco);
+        printf(" %d: %s \n", i + 1, d->topicos[i].nome_topico);
+        pthread_mutex_unlock(d->ptrinco);
+    }
+
+    //printf("\n\n");
+}
+
 int main(int argc, char *argv[]) {
     if (argc != 1) {
         return 1;
     }
 
     // EXISTE SERVIDOR ON?(SO PODE EXISTIR UM)
-    if (access(FIFO_SERV,F_OK) == 0) {
+    if (access(FIFO_SERV_MSG,F_OK) == 0 || access(FIFO_SERV_MSG,F_OK) == 0) {
         printf("[ERRO]-SERVER ON\n");
         exit(3);
     }
@@ -193,12 +288,14 @@ int main(int argc, char *argv[]) {
     printf("ESPERANDO USERS...\n");
 
     // THREAD LOGIN
-    pthread_t thread_login;     // 1 THREAD POR CLI
+    pthread_t thread_login;    
     pthread_create(&thread_login, NULL, executa_login, &data);
     //...
 
     // MSG
 
+    pthread_t thread_msg;
+    pthread_create(&thread_msg, NULL, recebe_msg, &data);
     //...
 
     // CICLO PRINCIPAL
@@ -215,8 +312,13 @@ int main(int argc, char *argv[]) {
             continue;
         }
 
+        if (strcmp(cmd, "topics") == 0) {
+            mostra_topicos(&data);
+            continue;
+        }
+
         //RETIRA TODOS OS USER LIGADOS AO SERVER
-        if (strcmp(cmd, "remove all") == 0 || strcmp(cmd, "quit") == 0) {
+        if (strcmp(cmd, "remove all") == 0) {
 
             pthread_mutex_lock(data.ptrinco);
             int n = data.nUsers;
@@ -271,7 +373,7 @@ int main(int argc, char *argv[]) {
             continue;
         }
 
-        if (strcmp(cmd, "fecha") == 0) {
+        if (strcmp(cmd, "desligaL") == 0) {
             // Sinaliza que o login deve parar
             data.login_on = false;
 
@@ -297,13 +399,62 @@ int main(int argc, char *argv[]) {
             continue;
         }
 
+        if (strcmp(cmd, "desligaM") == 0) {
+            // Sinaliza que o login deve parar
+            data.msg_on = false;
+
+            MENSAGEM m;
+            strcpy(m.topico,"desliga");
+            m.pid = getpid();
+
+            int man_pipe_msg = open(FIFO_SERV_MSG, O_WRONLY);
+            write(man_pipe_msg, &m, sizeof(MENSAGEM));
+
+            // Aguarda a thread terminar
+            pthread_join(thread_login, NULL);
+
+            printf("[MAIN] Thread de login encerrada.\n");
+            continue;
+        }
+
+        if (strcmp(cmd, "quit") == 0) {
+
+            pthread_mutex_lock(data.ptrinco);
+            int n = data.nUsers;
+            pthread_mutex_unlock(data.ptrinco);
+
+            for (int i = n - 1; i >= 0; i--) {
+
+                pthread_mutex_lock(data.ptrinco);
+                // >>>>>>>>>>>>>ALTERAR "RESPOSTA"<<<<<<<<<<<<<
+                if (envia_info(data.users_pids[i], "QUIT")) {
+                    data.nUsers--;
+                } else {
+                    printf(" NAO DEU!\n");
+                }
+                pthread_mutex_unlock(data.ptrinco);
+
+            }
+
+            // Sinaliza que o login deve parar
+            data.login_on = false;
+
+            // Envia o sinal SIGUSR2 para a thread de login
+            pthread_kill(thread_login, SIGUSR2);
+
+            // Aguarda a thread terminar
+            pthread_join(thread_login, NULL);
+
+            printf("[MAIN] Thread de login encerrada.\n");
+            continue;
+        }
 
     } while (strcmp(cmd, "quit") != 0);
 
 
     // FECHA PIPES & APAGA FIFO
     //close(man_pipe);
-    unlink(FIFO_SERV);
+    unlink(FIFO_SERV_MSG);
     unlink(FIFO_SERV_LOG);
 
     printf("\nFIM MANAGER...\n");
